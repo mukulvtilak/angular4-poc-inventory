71732e0b7995910ef72bb067b5f71ac3896f85e5
mukulvtilak <tilak.mukul@hotmail.com>
2018-02-23 11:33:22 +0530

    pdf generation

--- src/client/app/about/about.component.ts
+++ src/client/app/about/about.component.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NameListService } from '../shared/name-list/name-list.service';
+
+// third party imports
+// import * as jspdf from 'jspdf';
+
+//const element = document.getElementById('print-content');
+// const pdf = new jspdf('p', 'pt','a4');
+const pdf = new jspdf();
+const docExtension: string = '.pdf';
 
 /**
  * This class represents the lazy loaded AboutComponent.
@@ -12,9 +20,23 @@ import { NameListService } from '../shared/name-list/name-list.service';
 })
 export class AboutComponent {
   oldData: string;
+  newData: string;
+  toPrint: object;
+  indentWith: string = '  '; // add \t if tab will be used for indentation, currently set to 2 whitespaces
+  docName: string = 'SampleDoc';
   constructor(dataService: NameListService) {
     dataService.getOldJson().subscribe(data => {
-      this.oldData = JSON.stringify(data, null, '\t');
+      this.oldData = JSON.stringify(data, null, this.indentWith);
+    });
+    dataService.getNewJson().subscribe(data => {
+      this.newData = JSON.stringify(data, null, this.indentWith);
+    });
+  }
+
+  printDoc(){
+    this.toPrint = document.getElementById('print-content');
+    pdf.addHTML(this.toPrint, () => {
+      pdf.save(this.docName + docExtension);
     });
   }
 
